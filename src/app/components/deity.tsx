26c705da31fc8a14a95c1b4c91bbe65186d43a46
Özgür Özbek <43455645+ozgurozbek@users.noreply.github.com>
2023-06-15 23:32:48 +0100

    Tutorial changes 2 - More component

--- src/app/components/deity.tsx
+++ src/app/components/deity.tsx
@@ -1,23 +1,39 @@
-function getText() {
-}
-
 function DeityImage() {
   return (
-    <img src="https://ozgurozbek.github.io/dnd/min_images/Jenelius_Londste.png"/>
+    <img className="rounded-full" alt="Jenelius" src="https://ozgurozbek.github.io/dnd/min_images/Jenelius_Londste.png" />
   )
 }
 
 function DeityDescription() {
-  return(
+  return (
     <p>Demigoddess of devotion, usually associated with fanatic determination.</p>
   );
 }
 
+function DeityDomain() {
+  return (
+    <p>FONTAWESOME</p>
+  );
+}
+
+function DeityRow() {
+  return (
+  <div className="m-[auto]">
+    <div className="w-[10%] inline-block">
+      <DeityImage />
+    </div>
+    <div className="w-[80%] inline-block">
+      <DeityDescription />
+    </div>
+    <div className="w-[10%] inline-block">
+      <DeityDomain />
+    </div>
+  </div>
+  );
+}
+
 export default function Deity() {
-    return(
-      <div>
-        <DeityImage/>
-        <DeityDescription/>
-      </div>
-    );
-  }
+  return (
+    <DeityRow />
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Header from "./components/header"
 import Deity from "./components/deity"
 
 export default function Home() {
   return (
-    <main>
-    <Header />
-    <Deity />
+    <main className="w-[1024px]">
+      <Deity />
     </main>
   )
 }
