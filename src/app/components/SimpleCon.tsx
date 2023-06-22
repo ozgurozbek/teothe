d9848a28029dfc7396e1d250dd85bc78aa7f6f9c
Özgür Özbek <43455645+ozgurozbek@users.noreply.github.com>
2023-06-22 23:07:14 +0100

    Update SimpleCon.tsx
    Co-Authored-By: god-hephaestus <111083432+god-hephaestus@users.noreply.github.com>

--- src/app/components/SimpleCon.tsx
+++ src/app/components/SimpleCon.tsx
@@ -5,12 +5,12 @@ const { Title } = Typography;
 export default function SimpleContent({ contentProps }) {
     let imgData;
     if (contentProps.image) {
-        imgData = <img className='w-[90%] my-4 rounded-lg' src={contentProps.image.src} alt={contentProps.image.alt} />
+        imgData = <img className='my-4 rounded-lg' src={contentProps.image.src} alt={contentProps.image.alt} />
     }
 
     let textData = [];
     for (let i = 0; i < contentProps.text.length; i++) {
-        textData.push(<p>{contentProps.text[i]}</p>)
+        textData.push(<p className='mb-2'>{contentProps.text[i]}</p>)
     }
 
     return (
