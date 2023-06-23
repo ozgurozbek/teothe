7ebe688e7f5254bb21359e470a68d5ad1ab0d717
Özgür Özbek <43455645+ozgurozbek@users.noreply.github.com>
2023-06-23 18:00:25 +0100

    Update TableMaterials.tsx

--- src/TableMaterials.tsx
+++ src/TableMaterials.tsx
@@ -1,18 +1,57 @@
 import { Table } from "antd";
+import SimpleContent from "./SimpleCon";
 
 export default function MaterialsTable() {
   const dataSource = [
     {
       key: '1',
-      name: 'Mike',
-      age: 32,
-      address: '10 Downing Street',
+      name: 'Mithral',
+      description: "No STR requirement. No stealth disadv. No light armor.",
     },
     {
       key: '2',
-      name: 'John',
-      age: 42,
-      address: '10 Downing Street',
+      name: 'Adamantine',
+      description: "Any critical hit to you becomes a normal hit.",
+    },
+    {
+      key: '3',
+      name: 'Lesser',
+      description: "-1 AC on armors. Prone to breaking.",
+    },
+    {
+      key: '4',
+      name: 'Frostforge',
+      description: "+1 AC on armors, vulnerable to warmth. Weapons deal cold damage.",
+    },
+    {
+      key: '5',
+      name: 'Skyforge',
+      description: "Removes any stealth disadvantage from armors, Grants +3 to hit on weapons.",
+    },
+    {
+      key: '6',
+      name: 'Demonic',
+      description: "Armor and Weapons glow with a dark hue and are hungry for blood. Armor might damage the wearer, weapons might heal the owner as the damage die on attack.",
+    },
+    {
+      key: '7',
+      name: 'Hexed',
+      description: "Armor cannot be damaged by normal attacks. Magic damage only. Weapons are always sharp.",
+    },
+    {
+      key: '8',
+      name: 'Phosite',
+      description: "Armor cannot be damaged by normal and magical attacks. Weapons cut through armor ignoring enemy AC.",
+    },
+    {
+      key: '9',
+      name: 'Cursed',
+      description: "The item belongs to a Warlock Patron. Consult with your DM.",
+    },
+    {
+      key: '10',
+      name: 'Blessed',
+      description: "The item is an artifact. Consult with your DM.",
     },
   ];
 
@@ -23,18 +62,19 @@ export default function MaterialsTable() {
       key: 'name',
     },
     {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
+      title: 'Description',
+      dataIndex: 'description',
+      key: 'description',
     },
   ];
 
-  return (<>
-    <Table dataSource={dataSource} columns={columns} />;
-  </>)
+  return (<section className="py-4">
+  <SimpleContent
+    contentProps={{
+      title: "Material Variants",
+      text: ["Your gear can only be infused once. Mithral & Adamantine can be used as is."]
+    }}
+  />
+  <Table dataSource={dataSource} columns={columns} pagination={false}/>
+</section>)
 }
