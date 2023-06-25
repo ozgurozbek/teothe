da52fa7e407eda68bf660754e4dcd9c0dbc0ad2b
Özgür Özbek <43455645+ozgurozbek@users.noreply.github.com>
2023-06-25 03:11:59 +0100

    Update TableNavigation to use ANTD

--- app/components/TableNavigation.tsx
+++ app/components/TableNavigation.tsx
@@ -1,34 +1,35 @@
+import { Button, Space } from "antd";
+
 function NavigateToTable(tableid) {
   document.getElementById(tableid)?.scrollIntoView();
 }
 
 export default function TablesNavigator() {
   return (
-    <section>
-      <button
-        className="mx-4"
-        onClick={() => {
-          NavigateToTable("resurrection");
-        }}
-      >
-        Resurrection
-      </button>
-      <button
-        className="mx-4"
-        onClick={() => {
-          NavigateToTable("materialvariants");
-        }}
-      >
-        Material Variants
-      </button>
-      <button
-        className="mx-4"
-        onClick={() => {
-          NavigateToTable("calendar");
-        }}
-      >
-        Calendar
-      </button>
-    </section>
+    <>
+      <Space wrap>
+        <Button
+          onClick={() => {
+            NavigateToTable("resurrection");
+          }}
+        >
+          Resurrection
+        </Button>
+        <Button
+          onClick={() => {
+            NavigateToTable("materialvariants");
+          }}
+        >
+          Material Variants
+        </Button>
+        <Button
+          onClick={() => {
+            NavigateToTable("calendar");
+          }}
+        >
+          Calendar
+        </Button>
+      </Space>
+    </>
   );
 }
