69ce301beae627a8870c95316d612dc2e1112780
Özgür Özbek <43455645+ozgurozbek@users.noreply.github.com>
2023-06-29 03:05:34 +0100

    Remove intro from Deities

--- pages/PageDeities.tsx
+++ pages/PageDeities.tsx
@@ -39,21 +39,6 @@ export default function DeitiesPage() {
       <Card bordered={false} className="w-full">
         <SimpleContent
           contentProps={{
-            title: "Intro",
-            text: [
-              'Long before their battles, gods were once "All," harnessing the energy and magic of reality. However, their egos led to conflicts and the need to determine superiority. Lesser creatures desired an end to the fighting, but gods, being immortal and unforgiving, couldn\'t accept another "Perfect Being\'s" actions.',
-              "Eventually, prayers were answered, and the gods ceased fighting, but this created a worse situation. They stopped communicating and individually created humans from Mceeju, who, despite their differences, served the gods. The gods made peace with their divergences and established various races within the Astral Sea.",
-              "Humans, lacking intelligence and divine guidance, possessed magic but often perished due to imperfections. Their deaths released magic into the universe. The gods accepted their punishment and judged themselves, losing their perfect nature and becoming distinct from one another.",
-              "The gods continued to create humans, who exhibited predictable yet mysterious traits. They showed mercy and guided some humans towards greatness, rewarding them with demigod status. However, such accomplishments became scarce over time.",
-              "Despite sharing the same creators, races clashed or multiplied uncontrollably, losing control of the Weave and facing an unstoppable force: Mceeju.",
-              "Mceeju divided the Astral Sea, creating the Material Planes and the Inner Worlds while isolating the Elemental Chaos with a dimensional barrier. Races were divided, and even the gods' domains were split. The Overworld, Underworld, and Outer Worlds emerged.",
-              "With newfound freedom, the gods led by Mceeju's order. Some stayed united, some pursued separate paths, and others descended to the Material Planes.",
-            ],
-          }}
-        />
-        <Divider />
-        <SimpleContent
-          contentProps={{
             title: "Overdeities",
           }}
         />
