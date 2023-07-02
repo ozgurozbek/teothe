4054b147f47b16af79daeb1dc305e7ab8e403d51
god-hephaestus <111083432+god-hephaestus@users.noreply.github.com>
2023-07-02 08:55:38 +0300

    fix and update Races Page

--- pages/PageRaces.tsx
+++ pages/PageRaces.tsx
@@ -11,11 +11,15 @@ export default function RacesPage() {
           RacePic={{
             src: "https://ozgurozbek.github.io/dnd/min_images/race_images/human.png",
             alt: "human",
+            phrase:
+              "humanhum anhuma nhumanh umanhumanhum anhumanh umanhumanhuman nhumanhu maanhuman human humanhuman",
           }}
           DescriptionText={{
             title: "Human",
-            paragraph:
+            paragraph: [
               "Humans were widespread, could be found in most regions and, in general, were fierce and disagreeable, which could sometimes lead certain other races to view them with contempt. They were renowned for their diversity and ambition, and although they lacked specialization, they could excel in many areas. While there were no natural human subraces, planetouched subspecies of humans were found in the Realms. These included the aasimars, genasi, and tieflings. There was also great ethnic diversity between humans in different regions. Humans bred outside their race many times, resulting in the various half-human races like half-elves and half-orcs.",
+            ],
+            homebrew: "HUAMNNNNNNNN",
           }}
         />
         <Divider />
--- src/components/RacesCon.tsx
+++ src/components/RacesCon.tsx
@@ -1,4 +1,4 @@
-import { Typography, Col, Row, Card, Image } from "antd";
+import { Typography, Col, Row, Image } from "antd";
 
 const { Title } = Typography;
 
@@ -6,20 +6,31 @@ export default function RacesCon({
   RacePic,
   DescriptionText,
 }: {
-  RacePic: { src: string; alt: string };
-  DescriptionText: { title: string; paragraph: string };
+  RacePic: { src: string; alt: string; phrase: string };
+  DescriptionText: { title: string; paragraph: string[]; homebrew: string };
 }) {
+  let textData = [];
+  {
+    for (let i = 0; i < DescriptionText.paragraph.length; i++) {
+      textData.push(<p className="mb-1">{DescriptionText.paragraph[i]}</p>);
+    }
+  }
+
   return (
-    <Row>
-      <Col flex={2}>
-        <Image src={RacePic.src} alt={RacePic.alt} rootClassName="rounded" />
+    <Row justify="space-between">
+      <Col span={6}>
+        <Col span={24}>
+          <Image src={RacePic.src} alt={RacePic.alt} />
+        </Col>
+        <Col>
+          <p className="italic text-center pt-2 text-lg">{RacePic.phrase}</p>
+        </Col>
       </Col>
-      <Col flex={3}>
+      <Col span={18}>
         <Title>{DescriptionText.title}</Title>
-        <p className="mb-2">{DescriptionText.paragraph}</p>
+        {textData}
+        <p className="pt-2 text-pink-600"> {DescriptionText.homebrew}</p>
       </Col>
     </Row>
   );
 }
-// cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-// add phrase to RacePic
