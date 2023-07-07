904451ebbb32c98dfc83b92f256138011d1e3908
Özgür Özbek <43455645+ozgurozbek@users.noreply.github.com>
2023-07-07 07:53:00 +0100

    Remove ugly map hack, display maps with state
    This doesn't work well at all. State lags so much when displaying page, and sometimes doesn't even render.
    
    Also because the images are absolute, they ignore screen height, card looks bad, no padding on card ends.

--- pages/PageMapOverlay.tsx
+++ pages/PageMapOverlay.tsx
@@ -1,18 +1,21 @@
 import { useState } from "react";
 import { Button, Card, Image, Space } from "antd";
 import GetCrumbs from "Comp/NavigationCrumb";
+import SimpleContent from "@/components/SimpleCon";
 
 export default function MapOverlayPage() {
-  const [maps, setMapVis] = useState("1");
-  let temp: any;
+  const [maps, setMapVis] = useState(["Base"]);
+  let temp: string[] = [];
 
   function handleVis(data: string) {
-    // This is not an array, it is an ugly substring hack
-    if (!maps.includes(data)) {
-      temp = maps + data;
+    temp = maps;
+    if (maps.includes(data)) {
+      const index = temp.indexOf(data);
+      if (index > -1) {
+        temp.splice(index, 1);
+      }
     } else {
-      temp = maps.split(data);
-      temp = temp[0] + temp[1];
+      temp.push(data);
     }
     setMapVis(temp);
   }
@@ -24,42 +27,108 @@ export default function MapOverlayPage() {
         <Image
           rootClassName="absolute"
           preview={false}
-          src={"./map" + maps[i] + ".png"}
+          src={"./Maps/" + maps[i] + ".png"}
           alt={maps[i]}
         />
       );
     }
-    return <div className="mt-4 h-[400px]">{mapData}</div>;
+    return <div className="mt-4">{mapData}</div>;
   }
 
   return (
     <section>
       <GetCrumbs path={"Teothe3K,Map Overlay"} />
-      <Card bordered={false} className="w-full">
+      <Card bordered={false} className="w-full min-h-[110vh]">
+        <SimpleContent
+          contentProps={{
+            title: "Map Overlay",
+            text: [
+              "Map overlays change their index in the order of the button press. You can add or remove overlays. There are obviously not HD, and small campuses or travel routes may provide difficult to see.",
+            ],
+          }}
+        />
         <Space wrap>
           <Button
             onClick={() => {
-              handleVis("2");
+              handleVis("Campuses");
             }}
           >
-            Mountain Overlay
+            Campuses Overlay
           </Button>
           <Button
             onClick={() => {
-              handleVis("3");
+              handleVis("Climates");
             }}
           >
-            Water Overlay
+            Climates Overlay
           </Button>
           <Button
             onClick={() => {
-              handleVis("4");
+              handleVis("Compass");
             }}
           >
-            City Overlay
+            Compass Overlay
+          </Button>
+          <Button
+            onClick={() => {
+              handleVis("Continents");
+            }}
+          >
+            Continents Overlay
+          </Button>
+          <Button
+            onClick={() => {
+              handleVis("Echoes");
+            }}
+          >
+            Echoes Overlay
+          </Button>
+          <Button
+            onClick={() => {
+              handleVis("Kingdoms");
+            }}
+          >
+            Kingdoms Overlay
+          </Button>
+          <Button
+            onClick={() => {
+              handleVis("Places");
+            }}
+          >
+            Places Overlay
+          </Button>
+          <Button
+            onClick={() => {
+              handleVis("Resources");
+            }}
+          >
+            Resources Overlay
+          </Button>
+          <Button
+            onClick={() => {
+              handleVis("Steamcart");
+            }}
+          >
+            Steamcart Overlay
+          </Button>
+          <Button
+            onClick={() => {
+              handleVis("Trade");
+            }}
+          >
+            Trade Overlay
+          </Button>
+          <Button
+            onClick={() => {
+              handleVis("Travel");
+            }}
+          >
+            Travel Overlay
           </Button>
         </Space>
-        <ShowMaps />
+        <div className="relative">
+          <ShowMaps />
+        </div>
       </Card>
     </section>
   );
