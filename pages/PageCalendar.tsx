6b02e746ba2056ca87310c3e14bb060c703d845d
Özgür Özbek <43455645+ozgurozbek@users.noreply.github.com>
2023-07-13 22:11:31 +0100

    Calendar data
    Co-Authored-By: god-hephaestus <111083432+god-hephaestus@users.noreply.github.com>

---
+++ pages/PageCalendar.tsx
@@ -0,0 +1,20 @@
+import { Card, Col, Row } from "antd";
+import GetCrumbs from "Comp/NavigationCrumb";
+import SimpleContent from "@/components/SimpleCon";
+import CalendarTable from "@/components/calendar/CalendarTable";
+import { useState } from "react";
+
+export default function CalendarPage() {
+  const [monthName, setMonthName] = useState("Buibus");
+  const [year, setYear] = useState(27);
+
+  return (
+    <section>
+      <GetCrumbs path={"Teothe3K,Calendar"} />
+      <Card bordered={false} className="w-full">
+        <SimpleContent contentProps={{ title: "Calendar" }} />
+        <CalendarTable calendarProps={{ monthName: monthName, year: year }} />
+      </Card>
+    </section>
+  );
+}
---
+++ src/components/calendar/CalendarCell.tsx
@@ -0,0 +1,34 @@
+import { Card } from "antd";
+
+export default function CalendarCell(cellProps: {
+  dayName: string;
+  dayNumber: number;
+  moonPhase?: string;
+  holiday?: string;
+  sessionNote?: string;
+}) {
+  return (
+    <Card className="rounded-none">
+      <div className="h-12">
+        <span>{cellProps.dayName}</span>
+        <span className="float-right">
+          {cellProps.moonPhase} {cellProps.dayNumber}
+        </span>
+      </div>
+      {cellProps.holiday && (
+        <div className="bg-red-300 h-4">
+          <span>
+            <strong>{cellProps.holiday}</strong>
+          </span>
+        </div>
+      )}
+      {cellProps.sessionNote && (
+        <div className="bg-blue-300 h-20">
+          <span>
+            <strong>{cellProps.sessionNote}</strong>
+          </span>
+        </div>
+      )}
+    </Card>
+  );
+}
