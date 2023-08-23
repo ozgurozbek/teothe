480d25af3a7a4e1237baa14fb20a4c0cf46e49bc
god-hephaestus <111083432+god-hephaestus@users.noreply.github.com>
2023-08-23 15:53:47 +0300

    add dropdown to Languages

--- pages/PageLanguages.tsx
+++ pages/PageLanguages.tsx
@@ -1,6 +1,6 @@
 import GetCrumbs from "@/components/NavigationCrumb";
 import Language from "@/components/language/languages";
-import { Card, Skeleton } from "antd";
+import { Button, Card, Dropdown, Skeleton, MenuProps } from "antd";
 import useSWR from "swr";
 import { useState } from "react";
 
@@ -8,9 +8,48 @@ function GetLanguageData() {
   const [curLang, setCurLang] = useState("Abyssal");
   let query = curLang;
 
+  const handleLangClick: MenuProps["onClick"] = (e) => {
+    setCurLang(e.key);
+  };
+
+  const langItems: MenuProps["items"] = [
+    {
+      label: "Abyssal",
+      key: "Abyssal",
+    },
+    {
+      label: "Draconic",
+      key: "Draconic",
+    },
+    {
+      label: "Drow",
+      key: "Drow",
+    },
+    {
+      label: "Dwarvish",
+      key: "Dwarvish",
+    },
+  ];
+
+  const langProps = {
+    items: langItems,
+    onClick: handleLangClick,
+  };
+
+  function LanguagesDropdown() {
+    return (
+      <>
+        Language:{" "}
+        <Dropdown menu={langProps}>
+          <Button>{curLang}</Button>
+        </Dropdown>
+      </>
+    );
+  }
+
   const fetcher = (args: RequestInfo) => fetch(args).then((res) => res.json());
   const { data, error } = useSWR(
-    "http://localhost:5000/getLanguages?lang=" + query,
+    "http://localhost:5000/getLanguages?lang=" + query, //http://localhost:5000/getLanguages?head=True
     fetcher
   );
   if (error) {
@@ -33,7 +72,12 @@ function GetLanguageData() {
       );
     }
   }
-  return renderedLanguages;
+  return (
+    <>
+      <LanguagesDropdown />
+      {renderedLanguages}
+    </>
+  );
 }
 
 export default function LanguagesPage() {
