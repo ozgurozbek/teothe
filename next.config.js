2d9a7b9e5501a562d0d3b12f5a2d26e109213979
Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
2024-02-12 16:04:37 +0000

    Add next PWA module

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
-/** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
 const nextConfig = {
   output: "export",
   experimental: {
     nextScriptWorkers: false,
   },
+  // Next.js config options should be added here.
 };
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
