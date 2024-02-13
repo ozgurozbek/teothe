6d91a28894f49066e2ef0ca9396d7827b427a8cf
Özgür Özbek <43455645+ozgurozbek@users.noreply.github.com>
2024-02-13 18:02:25 +0000

    Remove redundant code from Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -115,7 +115,6 @@ export default function Navbar() {
           }}
           preview={false}
           onClick={() => {
-            window.history.pushState({ path: "/" }, "", "/");
             setselectedKeys([]);
             render(
               <>
