15dec4e92702ce26e8f82ae3d641f85e7fa4f46c
Özgür Özbek <43455645+ozgurozbek@users.noreply.github.com>
2024-02-16 02:24:01 +0000

    Add runtimeCaching to PWA, hide development logs
    https://ducanh-next-pwa.vercel.app/docs/next-pwa/configuring#runtime-caching

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
+const runtimeCaching = require('next-pwa/cache');
 const withPWA = require("next-pwa")({
   dest: "public",
+  runtimeCaching,
 });
+
 
 const nextConfig = {
   output: "standalone", //Optimization in Calendar fails in Export, hence to be able to build...
   experimental: {
     nextScriptWorkers: false,
   },
+
   // Next.js config options should be added here.
 };
-module.exports = withPWA(nextConfig);
+module.exports = process.env.NODE_ENV === "development" ? nextConfig : withPWA(nextConfig); //Disables PWA on development, will throw [PWA] PWA support is disabled. See module.exports
