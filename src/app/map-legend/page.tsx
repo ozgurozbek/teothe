cef4ee80dc9981af49d1edbc3eab36ad88c5e9c6
god-hephaestus <111083432+god-hephaestus@users.noreply.github.com>
2024-04-10 16:27:42 +0300

    restructure every page to import from client components

--- src/app/map-legend/page.tsx
+++ src/app/map-legend/page.tsx
@@ -1,83 +1,11 @@
-import MapEntry from "Comp/maps/MapEntry";
-import { Card, Skeleton } from "antd";
-import GetCrumbs from "Comp/NavigationCrumb";
-import SimpleContent from "Comp/SimpleCon";
-import useSWR from "swr";
-import Link from "next/link";
+import MapLegendContent from "@/components/maps/MapLegendContent";
 import { Metadata } from "next";
-
-/**
- * Component to get and render the map legend entries.
- * @generator
- * @see MapEntry
- * @returns JSX elements representing the map legend entries.
- */
-function GetMapLegend() {
-  /**
-   * Fetcher function for API requests.
-   * @param args - RequestInfo object containing information about the request.
-   * @returns Promise resolving to the parsed JSON response.
-   */
-  const fetcher = (args: RequestInfo) => fetch(args).then((res) => res.json());
-  const { data, error } = useSWR(
-    "https://gi5vwiheg0.execute-api.eu-central-1.amazonaws.com/Stage/getMapRes",
-    fetcher
-  );
-  if (error) {
-    console.log(error);
-    return <div>Failed to access API</div>;
-  }
-  if (!data) return <Skeleton active />;
-
-  let renderedMapRes = [];
-  for (let item of data) {
-    renderedMapRes.push(
-      //name type check image
-      <MapEntry
-        imageSrc={"./Maps/Legends/" + item.name + ".png"}
-        textProps={{
-          name: item.name,
-          rarity: item.type,
-          check: item.dice,
-        }}
-      />
-    );
-  }
-  return renderedMapRes;
-}
 
 export const metadata: Metadata = {
   title: "Map Legend",
   description:
     "Decode the symbols of Teothe's maps with our Map Legend, providing clarity and understanding to the geography of the Material Plane.",
 };
-/**
- * Component that asynchronously fetches map legend data from an API and renders map legend entries.
- * @returns An array of JSX elements representing map legend entries.
- */
 export default function MapLegendPage() {
-  return (
-    <section>
-      <GetCrumbs path={"Teothe,Map Legend"} />
-      <Card bordered={false} className="w-full">
-        <SimpleContent
-          contentProps={{
-            title: "Map Legend",
-            text: [
-              "Resource based legend for trader campaigns, descriptions and difficulty classes for all of them. These valuables can be found across Teothe.",
-              "Adds new skills named Artifice and Influence. Difficulty classes are averaged out. They can change depending on the situation and in where you are in Teothe.",
-            ],
-          }}
-        />
-        {GetMapLegend()}
-        <br />
-        <Link
-          href={"https://ozgurozbek.github.io/dnd/MapResourcesDoc.pdf"}
-          target="_blank"
-        >
-          See the mechanics document here.
-        </Link>
-      </Card>
-    </section>
-  );
+  return <MapLegendContent />;
 }
--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -1,111 +1,12 @@
-import { Card, Skeleton } from "antd";
-import GetCrumbs from "Comp/NavigationCrumb";
-import useSWR from "swr";
-import { Table } from "antd";
-import type { ColumnsType } from "antd/es/table";
+import ToolsContent from "@/components/tools/ToolsContent";
 import { Metadata } from "next";
-import SimpleContent from "@/components/SimpleCon";
-
-interface DataType {
-  key: React.Key;
-  title: string;
-  dataIndex: string;
-  cost: string;
-}
 
 export const metadata: Metadata = {
   title: "Tools",
   description:
     "Explore our suite of tools that empover, provide utility and add flavor to your character. Learn about how you can acquire and use them.",
 };
-/**
- * Function to fetch and display player tool data from the API.
- * @generator
- * @returns The table displaying information about player tools.
- */
-function GetTableData() {
-  /**
-   * Fetcher function for API requests.
-   * @param args - RequestInfo object containing information about the request.
-   * @returns Promise resolving to the parsed JSON response.
-   */
-  const fetcher = (args: RequestInfo) => fetch(args).then((res) => res.json());
-  const { data, error } = useSWR(
-    "https://gi5vwiheg0.execute-api.eu-central-1.amazonaws.com/Stage/getTools",
-    fetcher
-  );
-  if (error) {
-    console.log(error);
-    return <div>Failed to access API</div>;
-  }
-  if (!data) return <Skeleton active />;
 
-  //dataSource, columns
-  let columns: ColumnsType<DataType> = [];
-  columns.push(
-    {
-      title: "Player Tool",
-      dataIndex: "tool",
-      key: "tool",
-    },
-    {
-      title: "Proficiency Bonus",
-      dataIndex: "proficiency",
-      key: "proficiency",
-    },
-    {
-      title: "Exclusive Benefits",
-      dataIndex: "allows",
-      key: "allows",
-    },
-    {
-      title: "Cost",
-      dataIndex: "cost",
-      key: "cost",
-    }
-  );
-
-  let dataSource = [];
-  for (let rowData of data) {
-    let item: any = {};
-    item["tool"] = rowData.tool;
-    item["proficiency"] = rowData.proficiency;
-    item["allows"] = rowData.allows;
-    item["cost"] = rowData.cost;
-    dataSource.push(item);
-  }
-  return (
-    <>
-      <SimpleContent
-        contentProps={{
-          title: "Tools",
-          text: [
-            "This is a reference for player tools. Keep in mind that certain situations ask for different rolls, and certain prerequisities sometimes won't be met. Arguably, this table is good for referencing and ideas, but is not always true.",
-          ],
-        }}
-      />
-      <Table
-        className="mt-4"
-        dataSource={dataSource}
-        columns={columns}
-        pagination={false}
-        scroll={{ x: 1200 }}
-      />
-    </>
-  );
-}
-
-/**
- * Component for the tools page, displaying breadcrumbs and a card containing table data.
- * @returns The tools page section with breadcrumbs and table data.
- */
 export default function ToolsPage() {
-  return (
-    <section>
-      <GetCrumbs path={"Teothe,Tools"} />
-      <Card bordered={false} className="w-full">
-        {GetTableData()}
-      </Card>
-    </section>
-  );
+  return <ToolsContent />;
 }
