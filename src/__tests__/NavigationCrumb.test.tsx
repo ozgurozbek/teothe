a54e4bd67fcd8d45ffd653bf5d79646b726bb7e9
Özgür Özbek <43455645+ozgurozbek@users.noreply.github.com>
2024-07-19 16:50:14 +0100

    Test NavigationCrumb

---
+++ src/__tests__/NavigationCrumb.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import GetCrumbs from "../components/NavigationCrumb";
+
+describe("GetCrumbs", () => {
+    it("displays breadcrumbs individually", () => {
+        render(<GetCrumbs path="Teothe,Test Crumb" />);
+        
+        // Check each part of the breadcrumb
+        expect(screen.getByText("/ Teothe")).toBeInTheDocument;
+        expect(screen.getByText("/ Test Crumb")).toBeInTheDocument;
+    });
+});
