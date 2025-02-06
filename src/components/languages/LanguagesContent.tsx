586d700ac3629129f4eca041f5bf486e40aa3734
god-hephaestus <111083432+god-hephaestus@users.noreply.github.com>
2025-02-07 00:59:27 +0300

    LanguagesContent jsons
    refactoring isn't complete
    
    @ozgurozbek

--- src/components/languages/LanguagesContent.tsx
+++ src/components/languages/LanguagesContent.tsx
@@ -14,6 +14,24 @@ import {
 import { useState, useEffect } from "react";
 import { DownOutlined } from "@ant-design/icons";
 import SimpleContent from "@/components/SimpleCon";
+import elven from "@/jsons/languages/Elven.json";
+import gith from "@/jsons/languages/Gith.json";
+import giant from "@/jsons/languages/Giant.json";
+import draconic from "@/jsons/languages/Draconic.json";
+import halfancient from "@/jsons/languages/HalfAncient.json";
+import goblin from "@/jsons/languages/Goblin.json";
+import abyssal from "@/jsons/languages/Abyssal.json";
+import orcish from "@/jsons/languages/Orcish.json";
+import drow from "@/jsons/languages/Drow.json";
+import aokin from "@/jsons/languages/Aokin.json";
+import undercommon from "@/jsons/languages/Undercommon.json";
+import yuanti from "@/jsons/languages/YuanTi.json";
+import common from "@/jsons/languages/Common.json";
+import sylvan from "@/jsons/languages/Sylvan.json";
+import gnoll from "@/jsons/languages/Gnoll.json";
+import gnome from "@/jsons/languages/Gnome.json";
+import dwarvish from "@/jsons/languages/Dwarvish.json";
+
 /**
  * Functional component for the LanguagesDropdown.
  * @param curLang - The current language.
@@ -34,7 +52,25 @@ function LanguagesDropdown({
     items: langList,
     onClick: handleLangClick,
   };
-
+  langList = [
+    "elven",
+    "gith",
+    "giant",
+    "draconic",
+    "halfancient",
+    "goblin",
+    "orcish",
+    "drow",
+    "aokin",
+    "undercommon",
+    "yuanti",
+    "sylvan",
+    "common",
+    "gnoll",
+    "gnome",
+    "abyssal",
+    "dwarvish",
+  ];
   return (
     <Space className="mb-4">
       Language:{" "}
@@ -70,72 +106,36 @@ function LanguagesDropdown({
  * @returns JSX elements representing the LanguagesPage.
  */
 export default function LanguagesContent() {
-  const [curLang, setCurLang] = useState("Abyssal");
-  const [langList, setLangList] = useState<MenuProps["items"]>([]);
+  const [curLang, setCurLang] = useState("Elven");
   const [data, setData] = useState([]);
-  const [error, setError] = useState(null);
 
   const handleLangClick = (e: any) => {
     setCurLang(e.key);
   };
 
+  const langData: any = {
+    elven,
+    gith,
+    giant,
+    draconic,
+    halfancient,
+    goblin,
+    orcish,
+    drow,
+    aokin,
+    undercommon,
+    yuanti,
+    sylvan,
+    common,
+    gnoll,
+    gnome,
+    abyssal,
+    dwarvish,
+  };
+
   useEffect(() => {
-    (args: RequestInfo) => fetch(args).then((res) => res.json());
-
-    fetch(
-      "https://gi5vwiheg0.execute-api.eu-central-1.amazonaws.com/Stage/getLanguages?head=True"
-    )
-      .then((res) => res.json())
-      .then((langItemsData) => {
-        const langListTemp = langItemsData.map((item: string[]) => ({
-          label: item,
-          key: item,
-        }));
-        setLangList(langListTemp);
-      })
-      .catch((error) => {
-        console.log(error);
-        setError(error);
-      });
-  }, []);
-
-  useEffect(() => {
-    (args: RequestInfo) => fetch(args).then((res) => res.json());
-
-    fetch(
-      "https://gi5vwiheg0.execute-api.eu-central-1.amazonaws.com/Stage/getLanguages?lang=" +
-        curLang
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-      })
-      .catch((error) => {
-        console.log(error);
-        setError(error);
-      });
+    setData(langData[curLang]); // Retrieve the correct dataset from the object
   }, [curLang]);
-
-  if (error) {
-    return (
-      <Card bordered={false} className="w-full">
-        <SimpleContent
-          contentProps={{
-            title: "Languages",
-            text: [
-              "The Languages tab of Teothe serves as a comprehensive resource for Dungeons & Dragons (D&D), focusing on the exploration of fantasy languages. This feature offers a diverse array of languages, complete with both official and homebrew words, tailored specifically for enhancing the D&D 5e experience.",
-              "These are the language entries. Entries in white are from original Forgotten Realms content. Pinks are homebrew entries made specifically for Teothe. Pink and italic entries are adapted from original content to Teothe. Upon selecting a language from the dropdown menu below, the respective dictionary is displayed, showcasing a detailed collection of terms.",
-              "In the world of D&D, language plays a crucial role in storytelling and character development, providing insight into the unique cultures and histories of various fantasy realms. Teothe showcases an extensive collection of official words recognized within the D&D lexicon alongside homebrew words created by the community. This combination allows for a richer gameplay experience, enabling players and Dungeon Masters to customize their campaigns with personalized vocabulary.",
-              "The dictionary entries include meanings and contextual uses, making this resource invaluable for enriching character dialogues and infusing campaigns with deeper lore. The integration of both official and homebrew terms encourages creativity, expanding storytelling possibilities and enhancing immersion in the D&D universe.",
-              "Teothe transforms the language experience, offering a wealth of vocabulary to shape epic adventures within the realms of D&D. The richness of fantasy language serves to inspire and elevate gameplay, providing the tools needed to craft unforgettable narratives.",
-            ],
-          }}
-        />
-        <Divider />
-        Failed to fetch dropdown list data
-      </Card>
-    );
-  }
 
   if (data.length === 0) {
     return <Skeleton active />;
