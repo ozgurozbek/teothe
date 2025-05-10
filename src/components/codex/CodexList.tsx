e2fbf872e608a33222caf3dd5b929605a43b5d0d
Özgür Özbek <43455645+ozgurozbek@users.noreply.github.com>
2025-05-10 02:05:58 +0100

    Add descriptive text to Codex

--- src/components/codex/CodexList.tsx
+++ src/components/codex/CodexList.tsx
@@ -12,8 +12,10 @@ export default function CodexList({ posts }: { posts: any[] }) {
       <Card bordered={false} className="w-full">
         <SimpleContent
           contentProps={{
-            title: "title",
-            text: ["Text"],
+            title: "Codex",
+            text: [
+              "Dive into rich lore, uncover immersive stories, and get exclusive gameplay tips that deepen your understanding of the realm. Whether you're looking for the latest news, detailed character arcs, or behind-the-scenes fantasy worldbuilding, the Codex is where every path begins.",
+              "Stay updated with developer insights, strategy breakdowns, and curated Teothe articles; all crafted to enhance your journey through its evolving universe."],
           }}
         />
         <Divider />
