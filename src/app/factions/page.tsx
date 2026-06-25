98c3a540fcb161d8a174e7c405c1370df008a3f5
Özgür Özbek <43455645+ozgurozbek@users.noreply.github.com>
2026-06-25 22:48:02 +0100

    Revert package change
    This reverts commit 16b1676334040fa639eb5d307bde057d1d7063dc.
    Delete package-lock.json
    Revert "Delete package-lock.json"
    This reverts commit 47c81687880e693fb2571d52a9392f3b765e65a2.
    Update page.tsx

--- src/app/factions/page.tsx
+++ src/app/factions/page.tsx
@@ -4,7 +4,7 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Factions",
   description:
-    "Discover the influential factions of Teothe, their goals, alliances, and rivalries that shape Teothe’s politics, conflicts, and history.",
+    "Discover the influential factions of Teothe, their goals, alliances, and rivalries that shape Teothe's politics, conflicts, and history.",
 };
 
 export default function FactionsPage() {
